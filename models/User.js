e01888c04653c722c8a118b87bb94a0b44b19958
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-05-03 09:20:30 +0200

    added new schema entry to users

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const userSchema = new Schema({
     firstName: { type: String, required: [true, "First name is required"] },
     lastName: { type: String, required: [true, "Last name is required"] },
     email: { type: String, required: [true, "Email is required"], unique: true },
+    readingList: { type: Array, required: [false, ""], unique: true },
     createdAt: { type: Date, default: Date.now },
 });
 
