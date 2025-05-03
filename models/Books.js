a06d8edb18b6a7b69d8b87de77da6eae84a1fc62
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-05-03 09:56:45 +0200

    added book model

---
+++ models/Books.js
@@ -0,0 +1,10 @@
+import { Schema, model, ObjectId } from "mongoose";
+
+const bookSchema = new Schema({
+    title: { type: String, required: [true, "Title is required"] },
+    bookRefId: { type: Schema.Types.ObjectId, ref: "User" },
+    status: { type: String, required: [true, "Read status is required"] },
+    createdAt: { type: Date, default: Date.now },
+});
+
+export default model("Book", bookSchema);
