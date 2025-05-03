bce4c3be4908604525dc5c89fb46c57f6cb00edc
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-05-03 14:07:10 +0200

    added function to update user books in reading list

--- controllers/users.js
+++ controllers/users.js
@@ -47,7 +47,7 @@ export const deleteUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "User deleted" });
 });
 
-export const addBook = asyncHandler(async (req, res) => {
+export const addUserBook = asyncHandler(async (req, res) => {
     const {
         body: { readingList },
         params: { id },
@@ -56,3 +56,15 @@ export const addBook = asyncHandler(async (req, res) => {
     // console.log(req.body);
     res.status(200).json(user);
 });
+
+export const updateUserBook = asyncHandler(async (req, res) => {
+    const {
+        body: { readingList },
+        params: { id, bookId },
+    } = req;
+    const user = await User.updateOne({ _id: id, "readingList.bookRefId": bookId }, { $set: { "readingList.$.status": req.body[0].status } });
+    // console.log(readingList.status);
+    res.status(200).json(user);
+});
+
+export const deleteUserBook = asyncHandler(async (req, res) => {});
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createUser, deleteUser, getUserById, getUsers, updateUser, addBook } from "../controllers/users.js";
+import { createUser, deleteUser, getUserById, getUsers, updateUser, addUserBook, updateUserBook } from "../controllers/users.js";
 
 const userRouter = Router();
 
 userRouter.route("/").get(getUsers).post(createUser);
 userRouter.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
-userRouter.route("/:id/books").post(addBook);
+userRouter.route("/:id/books").post(addUserBook);
+userRouter.route("/:id/books/:bookId").put(updateUserBook);
 
 export default userRouter;
