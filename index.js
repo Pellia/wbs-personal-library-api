2a3c24eae50a8e66a3446b6e73560211b7a609a7
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-05-03 09:26:58 +0200

    added user controller

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+import "./db/index.js";
 import express from "express";
 import errorHandler from "./middleware/errorHandler.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.use("/", (req, res) => res.status(200).json({ message: "Hello World" }));
+// app.use("/", (req, res) => res.status(200).json({ message: "Hello World" }));
+app.use("/users", userRouter);
 app.use("/{*any}", (req, res) => res.status(404).json({ error: "Not Found" }));
 app.use(errorHandler);
 
